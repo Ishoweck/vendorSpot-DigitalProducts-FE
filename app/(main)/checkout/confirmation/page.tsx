09020eee85af767567ac63da63e21d312c9c8d1f
streaktv8@gmail.com <streaktv8@gmail.com>
2025-09-15 16:30:02 +0100

    paystack redirect

--- app/(main)/checkout/confirmation/page.tsx
+++ app/(main)/checkout/confirmation/page.tsx
@@ -1,10 +1,23 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import FullScreenLoader from "../../../../components/Loader";
+// import ProductsPage from "./ProductPage";
 import CheckoutConfirmationPage from "./CheckOutPage";
 
 export default function YourComponent() {
-  const isLoading = true; 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay (e.g., fetching data, verifying token, etc.)
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); 
+
+    return () => clearTimeout(timer);
+  }, []);
 
   if (isLoading) return <FullScreenLoader />;
 
-  return <><CheckoutConfirmationPage/> </>;
+  return <CheckoutConfirmationPage />;
 }
