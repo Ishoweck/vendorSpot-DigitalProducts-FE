de75bcb683099d7d51d7a578d8799ea99b71a3ee
streaktv8@gmail.com <streaktv8@gmail.com>
2025-09-16 22:10:16 +0100

    direct route linkong

---
+++ app/(main)/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function ProductDetailPageStub() {
+  const params = useParams();
+  console.log("params:", params);
+  return <h1>Param id: {String(params.id)}</h1>;
+}
--- app/(main)/products/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { useParams } from "next/navigation";
-export default function ProductDetailPage() {
-  return <h1>This is ProductDetailPage. Param id: {useParams().id}</h1>;
-}
